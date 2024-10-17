2637846838ca13007ef8cd2c51844fd03fd2f63b
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-17 10:29:42 -0300

    add codigo do exercicio 7, list_maior_idade

---
+++ 07_list_maior_idade.py
@@ -0,0 +1,16 @@
+#7. Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+
+lista: list = [18, 17, 14, 21, 25, 30]
+
+lista_maior = []
+lista_menor = []
+
+for idade in lista:
+    if idade >= 18:
+        lista_maior.append(idade)
+    else:
+        lista_menor.append(idade)
+
+
+
+print(lista_maior)
