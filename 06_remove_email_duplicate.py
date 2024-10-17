5f09316e9bef439ac9cc71c8bdb5a3861ba11aff
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-17 10:21:08 -0300

    add codigo do exercicio 06 remove duplicate

---
+++ 06_remove_email_duplicate.py
@@ -0,0 +1,8 @@
+#6. Dada uma lista de emails, remover todos os duplicados.
+
+
+lista: list = ['lucas@gmail.com', 'lucas@gmail.com', 'lais@gmail.com']
+
+# set remove duplicatas
+print(list(set(lista)))
+    
