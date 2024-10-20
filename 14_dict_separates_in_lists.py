1a77eaa9b880854e05cbae59960ead31fb283a4b
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-20 20:55:08 -0300

    add codigo ao exercicio 14, separar dicionario chave, valor em listas

---
+++ 14_dict_separates_in_lists.py
@@ -0,0 +1,10 @@
+#14. Dado um dicionário, criar listas separadas para suas chaves e valores.
+
+dicionario: dict = {'chave1': 'valor1', 'chave2': 'valor2'}
+
+
+list_keys:list  = dicionario.keys()
+list_values:list =  dicionario.values()
+
+print(list_keys)
+print(list_values)
